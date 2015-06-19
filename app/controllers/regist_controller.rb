9d54000b38b48baa815cebee5347108a1c6f2d8b
dejidigitalo <dejidigitalo@gmail.com>
2015-06-20 03:06:19 +0900

    test book

--- app/controllers/regist_controller.rb
+++ app/controllers/regist_controller.rb
@@ -4,4 +4,10 @@ class RegistController < ApplicationController
   def view
     @msg = 'こんにちは、世界！'
   end
+  def list
+    @books = Book.all
+  end
+  def app_var
+    render text: MY_APP['logo']['source']
+  end
 end
